e2600c866a8a2a53ccf10dd6b89e4a0a1b04ffb1
santoandre4444 <santo.andre4444@gmail.com>
2025-06-22 16:04:38 +0200

    messaggio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './globals.css'; // Assicurati che il CSS sia lì
 
 type Product = {
